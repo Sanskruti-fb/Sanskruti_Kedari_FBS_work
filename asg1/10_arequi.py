3904453079f53980e8650895e97b0fc5d2f8ef0e
Sanskruti-fb <sanskrutifbit@gmail.com>
2025-06-26 21:56:01 +0530

    Delete asg1 directory

--- asg1/10_arequi.py
+++
@@ -1,7 +0,0 @@
-# Write a program to calculate area of an equilateral triangle.
-
-side = float(input("Enter the length of the side of the equilateral triangle:"))
-
-area = (( 3**0.5)/4 )* (side*side)
-
-print("Area of equilateral triangle: ",area)
