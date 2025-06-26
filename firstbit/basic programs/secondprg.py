210fc4325b4cd310d2917d4d30c9fd4640cf315c
Sanskruti-fb <sanskrutifbit@gmail.com>
2025-06-26 21:52:04 +0530

    Delete firstbit directory

--- firstbit/basic programs/secondprg.py
+++
@@ -1,7 +0,0 @@
-p = int(input("Enter principle amount: "))
-years = int(input("Enter no. of years: "))
-rate = float(input("Enter rate of interest: "))
-
-interest = (p*years*rate)/100
-
-print("Simple interest: ",interest)
