8187b7ed0293c2ac2cee3819b375b4657207101e
Sanskruti-fb <sanskrutifbit@gmail.com>
2025-07-07 19:06:40 +0530

    Delete test1 directory

--- test1/simple.py
+++
@@ -1,7 +0,0 @@
-p = int(input("Enter the principle amount:"))
-r = float(input("Enter the rate of interest:"))
-t = int(input("Enter the number of times in year:"))
-
-simple_interest = p*r*t/100
-
-print("Simple interest for the data is:",simple_interest)
